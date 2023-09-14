e03a4710a7918a9ecabc8ae4a5cc9d0bb0dd6429
saikumar <saikumar@digisprint.com>
2023-09-14 10:18:14 +0530

    [feat] :setup project added assets.

---
+++ src/sections/SuperQuality.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SuperQuality = () => {
+  return (
+    <div>
+      SuperQuality
+    </div>
+  )
+}
+
+export default SuperQuality
+
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+
+export {
+    Hero,
+    PopularProduct,
+    Services,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+    Footer,
+    CustomerReviews
+}
